67e76f8784580ac0aa88fe9ab6403f093103e43d
Catherine Devlin <catherine.devlin@gsa.gov>
2017-01-20 14:19:57 -0500

    Separate awards_detail view to support /awards/<pk>

--- usaspending_api/awards/urls.py
+++ usaspending_api/awards/urls.py
@@ -7,18 +7,21 @@ award_summary_id_patterns = [
 ]
 
 # map reqest types to viewset method; replace this with a router
-award = views.AwardListViewSet.as_view(
+award_list = views.AwardListViewSet.as_view(
     {'get': 'list', 'post': 'list'})
+award_detail = views.AwardListViewSet.as_view(
+    {'get': 'retrieve', 'post': 'retrieve'})
 award_summary = views.AwardListSummaryViewSet.as_view({
     'get': 'list', 'post': 'list'})
 award_total = views.AwardListAggregateViewSet.as_view({
     'get': 'list', 'post': 'list'})
 
 urlpatterns = [
-    url(r'^$', award),
-    url(r'^uri/(?P<uri>\w+)', award),
-    url(r'^fain/(?P<fain>\w+)', award),
-    url(r'^piid/(?P<piid>\w+)', award),
+    url(r'$', award_list, name='award-list'),
+    url(r'/(?P<pk>[0-9]+)/$', award_detail, name='award-detail'),
+    # url(r'^uri/(?P<uri>\w+)', award),
+    # url(r'^fain/(?P<fain>\w+)', award),
+    # url(r'^piid/(?P<piid>\w+)', award),
     url(r'^summary/', include(award_summary_id_patterns)),
     url(r'^summary/', award_summary),
     url(r'^total/', award_total)
